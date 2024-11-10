e56e0183c7fd121a1586b18517a3170629cc51a3
WillC919 <william.p.lee@stonybrook.edu>
2024-11-09 23:37:14 -0500

    yay

--- app.js
+++ app.js
@@ -32,131 +32,126 @@ app.use(cookieParser());
 
 // Session setup
 app.use(session({
-    secret: 'jaws', // Change this to a random secret
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: 'mongodb://localhost:27017/milestone-1', // Your MongoDB connection string
-        ttl: 14 * 24 * 60 * 60 // = 14 days. Default
-    }),
-    cookie: {
-        maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
-    }
+  secret: 'jaws', // Change this to a random secret
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/milestone-1', // Your MongoDB connection string
+    ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+  }),
+  cookie: {
+    maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+  }
 }));
 
 const checkSession = async (req, res, next) => {
-    if (req.session && req.session.userId) {
-    	const user = await User.findById(req.session.userId);
-    	if (!user) { return res.status(200).json({ status: 'ERROR', error:true, message: "Not valid user" }); }        
-	    next();
-    } else {
-	    res.redirect('/login');
-    }
+  if (req.session && req.session.userId) {
+    const user = await User.findById(req.session.userId);
+    if (!user) { return res.status(200).json({ status: 'ERROR', error:true, message: "Not valid user" }); }        
+    next();
+  } else {
+    res.redirect('/login');
+  }
 };
 
 const checkSessionVideos = async (req, res, next) => {
-    if (req.session && req.session.userId) {
-        const user = await User.findById(req.session.userId);
-        if (!user) { return res.status(200).json({ status: 'ERROR', error:true, message: "Not valid user" }); }
-        next();
-    } else {
-        res.status(200).json({ status: 'ERROR', error: true, message: 'Not logged in' });
-    }
+  if (req.session && req.session.userId) {
+    const user = await User.findById(req.session.userId);
+    if (!user) { return res.status(200).json({ status: 'ERROR', error:true, message: "Not valid user" }); }
+    next();
+  } else {
+    res.status(200).json({ status: 'ERROR', error: true, message: 'Not logged in' });
+  }
 };
-
 
 app.get('/', checkSession, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html')); // Serve the dashboard-homepage
 });
 
-app.use('/media/:id', checkSession, async (req, res) => {
-    console.log("Requested parameters:", req.params.id);
+// app.use('/media/:id', checkSession, async (req, res) => {
+//     console.log("Requested parameters:", req.params.id);
     
-    let videoId = req.params.id;
-    let filePath;
+//     let videoId = req.params.id;
+//     let filePath;
   
-    try {
-      if (videoId.endsWith('.m4s')) {
-        // Handling .m4s segment files
-        filePath = path.join(__dirname, 'media', videoId);
-        console.log("Serving .m4s file:", filePath);
+//     try {
+//       if (videoId.endsWith('.m4s')) {
+//         // Handling .m4s segment files
+//         filePath = path.join(__dirname, 'media', videoId);
+//         console.log("Serving .m4s file:", filePath);
   
-        // Set Content-Type for .m4s files
-        res.setHeader('Content-Type', 'video/mp4');
-      } else {
-        // Handling .mpd manifest files
-        const trimmedVideoId = videoId.slice(0, -4); // Remove .mpd extension
-        console.log("Processed video ID:", trimmedVideoId);
+//         // Set Content-Type for .m4s files
+//         res.setHeader('Content-Type', 'video/mp4');
+//       } else {
+//         // Handling .mpd manifest files
+//         const trimmedVideoId = videoId.slice(0, -4); // Remove .mpd extension
+//         console.log("Processed video ID:", trimmedVideoId);
   
-        // Fetch video details from the database
-        const video = await Video.findById(trimmedVideoId);
-        if (!video) {
-          return res.status(404).json({
-            status: 'ERROR',
-            error: true,
-            message: 'Video not found'
-          });
-        }
+//         // Fetch video details from the database
+//         const video = await Video.findById(trimmedVideoId);
+//         if (!video) {
+//           return res.status(404).json({
+//             status: 'ERROR',
+//             error: true,
+//             message: 'Video not found'
+//           });
+//         }
   
-        // Construct the path for the .mpd file based on the video title
-        const title = video.title.endsWith('.mp4') ? video.title.slice(0, -4) : video.title;
-        filePath = path.join(__dirname, 'media', `${title}.mpd`);
-        console.log("Serving .mpd file:", filePath);
+//         // Construct the path for the .mpd file based on the video title
+//         const title = video.title.endsWith('.mp4') ? video.title.slice(0, -4) : video.title;
+//         filePath = path.join(__dirname, 'media', `${title}.mpd`);
+//         console.log("Serving .mpd file:", filePath);
   
-        // Check if the file exists
-        if (!fs.existsSync(filePath)) {
-          return res.status(404).json({
-            status: 'ERROR',
-            error: true,
-            message: 'File not found on server'
-          });
-        }
+//         // Check if the file exists
+//         if (!fs.existsSync(filePath)) {
+//           return res.status(404).json({
+//             status: 'ERROR',
+//             error: true,
+//             message: 'File not found on server'
+//           });
+//         }
   
-        // Set Content-Type for .mpd files
-        res.setHeader('Content-Type', 'application/dash+xml');
-      }
+//         // Set Content-Type for .mpd files
+//         res.setHeader('Content-Type', 'application/dash+xml');
+//       }
   
-      // Send the file to the client
-      res.sendFile(filePath, (err) => {
-        if (err) {
-          console.error("Error sending file:", err);
-          res.status(500).end();
-        } else {
-          console.log("File sent successfully.");
-        }
-      });
+//       // Send the file to the client
+//       res.sendFile(filePath, (err) => {
+//         if (err) {
+//           console.error("Error sending file:", err);
+//           res.status(500).end();
+//         } else {
+//           console.log("File sent successfully.");
+//         }
+//       });
       
-    } catch (err) {
-      console.error("Error:", err);
-      res.status(500).json({
-        status: 'ERROR',
-        error: true,
-        message: 'An error occurred while retrieving the video file'
-      });
-    }
-  });
+//     } catch (err) {
+//       console.error("Error:", err);
+//       res.status(500).json({
+//         status: 'ERROR',
+//         error: true,
+//         message: 'An error occurred while retrieving the video file'
+//       });
+//     }
+//   });
 
-// app.use('/media', checkSession, express.static(path.join(__dirname, 'media')));
+app.use('/media', checkSession, express.static(path.join(__dirname, 'media')));
 
 // Route to serve the MPEG-DASH manifest
 app.get('/media/output.mpd', checkSession, (req, res) => {
-    res.sendFile(path.join(__dirname, 'media', 'output.mpd'));
+  res.sendFile(path.join(__dirname, 'media', 'output.mpd'));
 });
-
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
-
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
-
 app.get('/play/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'play.html'));
+  res.sendFile(path.join(__dirname, 'public', 'play.html'));
 });
-
 app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+  res.sendFile(path.join(__dirname, 'public', 'upload.html'));
 });
 
 // Routes
